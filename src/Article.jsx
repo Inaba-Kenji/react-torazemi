42a9bf85dfc009e77e183b3dd90968a3673f823d
Inaba Kenji <k.inaba040332@gmail.com>
2020-12-29 13:58:03 +0900

    add: react-torazemi8

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import LiKeButton from "./LikeButton";
 
 const Article = (props) => {
+  const [isPublished, togglePublished] = useState(false)
   return (
     <div>
       <h2>{props.title}</h2>
       <label htmlFor="check">公開状態:</label>
-      <input type="checkbox" checked={props.isPublished} id="check" onClick={() => props.toggle()}/>
+      <input type="checkbox" checked={isPublished} id="check" onClick={() => togglePublished(!isPublished)}/>
       <LiKeButton count={props.count} />
     </div>
   )
--- src/Blog.jsx
+++ src/Blog.jsx
@@ -3,48 +3,33 @@ import Article from './Article';
 import * as FooBar from './componets/FooBar';
 import Hoge from './componets/Hoge'
 
-class Blog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isPublished: false,
-      count: 0
-    }
-  }
+const Blog = () => {
 
-  componentDidMount() {
-    document.getElementById('counter').addEventListener('click', this.countUp)
-  }
 
-  componentDidUpdate() {
-    if (this.state.count >= 10) {
-        this.setState({count: 0})
-    }
-  }
+  // componentDidMount() {
+  //   document.getElementById('counter').addEventListener('click', this.countUp)
+  // }
 
-  componentWillUnmount() {
-    document.getElementById('counter').removeEventListener('click', this.countUp)
-  }
+  // componentDidUpdate() {
+  //   if (this.state.count >= 10) {
+  //       this.setState({count: 0})
+  //   }
+  // }
 
-  // 公開状態を反転させる関数
-  togglePublished = () => {
-    this.setState({
-      isPublished: !this.state.isPublished
-    })
-  };
+  // componentWillUnmount() {
+  //   document.getElementById('counter').removeEventListener('click', this.countUp)
+  // }
 
-  countUp = () => {
-    this.setState({count: this.state.count + 1 })
-  }
 
-  render () {
+
+  // countUp = () => {
+  //   this.setState({count: this.state.count + 1 })
+  // }
+
     return(
       <>
         <Article
          title={"Reactの使い方"}
-         isPublished={this.state.isPublished}
-         toggle={() => this.togglePublished()}
-         count={this.state.count}
         />
         <FooBar.Foo />
         <FooBar.Bar />
@@ -52,6 +37,5 @@ class Blog extends React.Component {
       </>
     )
   }
-};
 
 export default Blog;
